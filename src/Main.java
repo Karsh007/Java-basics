6386605688f28bc41867c02a8d573c413c53cd6f
karthik <karthikeya.nainala14@gmail.com>
2025-09-17 17:31:09 -0500

    changes

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        String item;
+//        double price;
+//        int quantity;
+//        char currency;
+//        double total;
+//
+//        System.out.print("what do you want 🍕   ");
+//        item = scanner.nextLine();
+//
+//        System.out.print("what is the price for each");
+//        price = scanner.nextDouble();
+//
+//        System.out.println("how many do you like");
+//        quantity = scanner.nextInt();
+//
+//        total = price * quantity;
+//
+//        System.out.println("this is total" + total);
+//        scanner.close();
+
+//        VARIABLES & DATA TYPES
+        int age = 25;
+        double salary = 55000;
+        boolean isEmployed = true;
+        String name = "Bhanu";
+
+        System.out.println("Name: " + name + ", Age: " + age + ", Salary: " + salary + ", Employed: " + isEmployed);
+
+        //OPERATORS
+        int a = 10, b = 3;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+        System.out.println("Is a > b? " + (a > b));
+
+
+
+
+    }
+}
